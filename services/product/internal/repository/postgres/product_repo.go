929bd0fba13f1a0b7dcd9bf276d2f05fc8573cca
DanilRoiven <106142708+danilpashin@users.noreply.github.com>
2026-04-09 17:49:25 +0500

    feat: add repo querys (product)

--- services/product/internal/repository/postgres/product_repo.go
+++ services/product/internal/repository/postgres/product_repo.go
@@ -4,6 +4,11 @@ import (
 	"apigateway/services/product/internal/domain"
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/Masterminds/squirrel"
 )
 
 type ProductRepository struct {
@@ -16,18 +21,63 @@ func NewPostgresProductRepository(db *sql.DB) *ProductRepository {
 
 func (r *ProductRepository) CreateProduct(ctx context.Context, insertData map[string]interface{}) (*domain.Product, error) {
 	var product domain.Product
+	builder := squirrel.Insert("products").
+		SetMap(insertData).
+		Suffix("RETURNING id, name, manufacturer, price, amount, created_at, updated_at").
+		PlaceholderFormat(squirrel.Dollar)
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, errors.New("invalid insert query")
+	}
+
+	err = r.db.QueryRowContext(ctx, query, args...).
+		Scan(&product.ID, &product.Name, &product.Manufacturer, &product.Price, &product.Amount, &product.CreatedAt, &product.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Product name is %s, created on %s\n", product.Name, product.CreatedAt)
 
 	return &product, nil
 }
 
 func (r *ProductRepository) UpdateProduct(ctx context.Context, id int, updateData map[string]interface{}) (*domain.Product, error) {
 	var product domain.Product
+	builder := squirrel.Update("products").
+		SetMap(updateData).
+		Where(squirrel.Eq{"id": id}).
+		Suffix("RETURNING id, name, manufacturer, price, amount, created_at, updated_at").
+		PlaceholderFormat(squirrel.Dollar)
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, errors.New("invalid update query")
+	}
+
+	err = r.db.QueryRowContext(ctx, query, args).
+		Scan(&product.ID, &product.Name, &product.Manufacturer, &product.Price, &product.Amount, &product.CreatedAt, &product.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Product name is %s, updated on %s\n", product.Name, product.UpdatedAt)
 
 	return &product, nil
 }
 
 func (r *ProductRepository) GetProduct(ctx context.Context, id int) (*domain.Product, error) {
 	var product domain.Product
+	builder := squirrel.Select("*").From("products").Where(squirrel.Eq{"id": id}).PlaceholderFormat(squirrel.Dollar)
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, errors.New("invalid get query")
+	}
+
+	err = r.db.QueryRowContext(ctx, query, args...).
+		Scan(&product.ID, &product.Name, &product.Manufacturer, &product.Price, &product.Amount, &product.CreatedAt, &product.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
 
 	return &product, nil
 }
@@ -35,9 +85,49 @@ func (r *ProductRepository) GetProduct(ctx context.Context, id int) (*domain.Pro
 func (r *ProductRepository) ListProducts(ctx context.Context, cursor int, limit uint64) ([]*domain.Product, error) {
 	listProducts := make([]*domain.Product, 0)
 
+	builder := squirrel.Select("*").From("products").
+		Where(squirrel.Gt{"id": cursor}).Limit(limit).OrderBy("id ASC").
+		PlaceholderFormat(squirrel.Dollar)
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, errors.New("invalid get list query")
+	}
+
+	rows, err := r.db.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, errors.New("error during the query")
+	}
+
+	for rows.Next() {
+		product := new(domain.Product)
+		err = rows.Scan(&product.ID, &product.Name, &product.Manufacturer, &product.Price, &product.Amount, &product.CreatedAt, &product.UpdatedAt)
+		if err != nil {
+			return nil, fmt.Errorf("scan error: %w", err)
+		}
+		listProducts = append(listProducts, product)
+	}
+	defer rows.Close()
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
 	return listProducts, nil
 }
 
 func (r *ProductRepository) DeleteProduct(ctx context.Context, id int) error {
+	builder := squirrel.Delete("products").Where(squirrel.Eq{"id": id}).PlaceholderFormat(squirrel.Dollar)
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return errors.New("invalid delete query")
+	}
+
+	err = r.db.QueryRowContext(ctx, query, args...).Scan()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
