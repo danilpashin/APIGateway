5ac16c49cad6cd0cf8953b34d9b5c6890b1758a6
DanilRoiven <106142708+danilpashin@users.noreply.github.com>
2026-04-26 18:32:44 +0500

    feat(user): add create user DTOs
    - Add CreateUserRequest and CreateUserResponse in user domain

--- services/user/internal/domain/user.go
+++ services/user/internal/domain/user.go
@@ -2,6 +2,7 @@ package domain
 
 import "time"
 
+// ===== USER =====
 type User struct {
 	ID           int       `json:"id"`
 	Username     string    `json:"username"`
@@ -10,4 +11,17 @@ type User struct {
 	Role         string    `json:"role"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+// ===== CREATE =====
+type CreateUserRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserResponse struct {
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
 }
