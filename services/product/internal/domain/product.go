775baf50622341f52de5ed8c1e30366e3c607c32
DanilRoiven <106142708+danilpashin@users.noreply.github.com>
2026-04-26 17:12:38 +0500

    refactor(product): remove unused db struct tags
    - Remove db tags from Product struct (using positional Scan instead)

--- services/product/internal/domain/product.go
+++ services/product/internal/domain/product.go
@@ -6,15 +6,15 @@ import (
 
 // ===== PRODUCT =====
 type Product struct {
-	ID           int       `json:"id" db:"id"`
-	Name         string    `json:"name" db:"name"`
-	Manufacturer string    `json:"manufacturer" db:"manufacturer"`
-	Price        int       `json:"price" db:"price"`
-	Amount       int       `json:"amount" db:"amount"`
-	Status       bool      `json:"status" db:"category"`
-	Category     string    `json:"category" db:"category"`
-	CreatedAt    time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at" db:"updated_at"`
+	ID           int       `json:"id"`
+	Name         string    `json:"name"`
+	Manufacturer string    `json:"manufacturer"`
+	Price        int       `json:"price"`
+	Amount       int       `json:"amount"`
+	Status       bool      `json:"status"`
+	Category     string    `json:"category"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 // ===== CREATE =====
