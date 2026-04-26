00289487eab13dab76247c168588fa58627c1f4c
DanilRoiven <106142708+danilpashin@users.noreply.github.com>
2026-04-26 19:28:53 +0500

    feat(user): implement CreateUser in UserRepository

--- services/user/internal/repository/postgres/user_repo.go
+++ services/user/internal/repository/postgres/user_repo.go
@@ -4,6 +4,9 @@ import (
 	"apigateway/services/user/internal/domain"
 	"context"
 	"database/sql"
+	"errors"
+
+	"github.com/Masterminds/squirrel"
 )
 
 type UserRepository struct {
@@ -15,7 +18,22 @@ func NewUserRepository(db *sql.DB) *UserRepository {
 }
 
 func (r *UserRepository) CreateUser(ctx context.Context, insertData map[string]interface{}) (*domain.User, error) {
-	return nil, nil
+	var user domain.User
+	builder := squirrel.Insert("users").
+		SetMap(insertData).
+		Suffix(`RETURNING id, username, email, password_hash, (SELECT name FROM roles WHERE id = 1) AS role_name, created_at, updated_at`).
+		PlaceholderFormat(squirrel.Dollar)
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return nil, errors.New("failed insert query build")
+	}
+
+	err = r.db.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.Username, &user.Email, &user.PasswordHash, &user.Role, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		return nil, errors.New("failed insert query")
+	}
+
+	return &user, nil
 }
 
 func (r *UserRepository) UpdateUser(ctx context.Context, id int, updateData map[string]interface{}) (*domain.User, error) {
