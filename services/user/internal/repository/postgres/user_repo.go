240966f152cdd6617b3fb1bf2c69b0be7b8db404
DanilRoiven <106142708+danilpashin@users.noreply.github.com>
2026-04-27 14:24:12 +0500

    fix(user): fix UpdateUser role issue in UserRepository
    - Change return value of update query from SELECT with default role_id = 1 to role_id only
    - Add second query to retrieve role name from roles by role_id

--- services/user/internal/repository/postgres/user_repo.go
+++ services/user/internal/repository/postgres/user_repo.go
@@ -38,14 +38,15 @@ func (r *UserRepository) CreateUser(ctx context.Context, insertData map[string]i
 
 func (r *UserRepository) UpdateUser(ctx context.Context, id int, updateData map[string]interface{}) (*domain.User, error) {
 	var user domain.User
-	builder := squirrel.Update("users").SetMap(updateData).Where(squirrel.Eq{"id": id}).Suffix(`RETURNING id, username, email, password_hash, (SELECT name FROM roles WHERE id = 1) AS role_name, created_at, updated_at`)
+	var role_id int
+	builder := squirrel.Update("users").SetMap(updateData).Where(squirrel.Eq{"id": id}).Suffix(`RETURNING id, username, email, password_hash, role_id, created_at, updated_at`)
 
 	query, args, err := builder.ToSql()
 	if err != nil {
 		return nil, errors.New("failed update query build")
 	}
 
-	err = r.db.QueryRowContext(ctx, query, args...).Scan(&user.ID)
+	err = r.db.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.Username, &user.Email, &user.PasswordHash, &role_id, &user.CreatedAt, &user.UpdatedAt)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, errors.New("user not found")
@@ -53,7 +54,17 @@ func (r *UserRepository) UpdateUser(ctx context.Context, id int, updateData map[
 		return nil, err
 	}
 
-	return nil, nil
+	query = `SELECT name FROM roles WHERE id = $1`
+
+	err = r.db.QueryRowContext(ctx, query, role_id).Scan(&user.Role)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errors.New("role not found")
+		}
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (r *UserRepository) GetUser(ctx context.Context, id int) (*domain.User, error) {
