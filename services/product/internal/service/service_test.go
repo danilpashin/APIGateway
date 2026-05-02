c456d2fee830528f2273659bb73f5291d2e25493
DanilRoiven <106142708+danilpashin@users.noreply.github.com>
2026-05-03 00:01:24 +0500

    test(product): add Update, Get, List, Delete tests for service

--- services/product/internal/service/service_test.go
+++ services/product/internal/service/service_test.go
@@ -82,22 +82,15 @@ var testsCreate = []TestCreate{
 		wantErr: false,
 	},
 	{
-		name: "missing name",
+		name:     "missing name",
+		input:    domain.CreateProductRequest{},
+		wantErr:  true,
+		wantResp: domain.ErrInvalidName,
+	},
+	{
+		name: "invalid name",
 		input: domain.CreateProductRequest{
-			Manufacturer: "test-manufacturer",
-			Price:        10000,
-			Amount:       10,
-			Status:       true,
-			Category:     "Household appliances",
-		},
-		want: &domain.Product{
-			ID:           1,
-			Name:         "",
-			Manufacturer: "test-manufacturer",
-			Price:        10000,
-			Amount:       10,
-			Status:       true,
-			Category:     "Household appliances",
+			Name: "t",
 		},
 		wantErr:  true,
 		wantResp: domain.ErrInvalidName,
@@ -105,14 +98,6 @@ var testsCreate = []TestCreate{
 	{
 		name: "missing manufacturer",
 		input: domain.CreateProductRequest{
-			Name:     "Test-product",
-			Price:    10000,
-			Amount:   10,
-			Status:   true,
-			Category: "Household appliances",
-		},
-		want: &domain.Product{
-			ID:       1,
 			Name:     "Test-product",
 			Price:    10000,
 			Amount:   10,
@@ -128,18 +113,6 @@ var testsCreate = []TestCreate{
 			Name:         "Test-product",
 			Manufacturer: "test-manufacturer",
 			Price:        -10000,
-			Amount:       10,
-			Status:       true,
-			Category:     "Household appliances",
-		},
-		want: &domain.Product{
-			ID:           1,
-			Name:         "Test-product",
-			Manufacturer: "test-manufacturer",
-			Price:        -10000,
-			Amount:       10,
-			Status:       true,
-			Category:     "Household appliances",
 		},
 		wantErr:  true,
 		wantResp: domain.ErrInvalidPrice,
@@ -150,18 +123,6 @@ var testsCreate = []TestCreate{
 			Name:         "Test-product",
 			Manufacturer: "test-manufacturer",
 			Price:        0,
-			Amount:       10,
-			Status:       true,
-			Category:     "Household appliances",
-		},
-		want: &domain.Product{
-			ID:           1,
-			Name:         "Test-product",
-			Manufacturer: "test-manufacturer",
-			Price:        0,
-			Amount:       10,
-			Status:       true,
-			Category:     "Household appliances",
 		},
 		wantErr:  true,
 		wantResp: domain.ErrInvalidPrice,
@@ -173,17 +134,6 @@ var testsCreate = []TestCreate{
 			Manufacturer: "test-manufacturer",
 			Price:        10000,
 			Amount:       -10,
-			Status:       true,
-			Category:     "Household appliances",
-		},
-		want: &domain.Product{
-			ID:           1,
-			Name:         "Test-product",
-			Manufacturer: "test-manufacturer",
-			Price:        10000,
-			Amount:       -10,
-			Status:       true,
-			Category:     "Household appliances",
 		},
 		wantErr:  true,
 		wantResp: domain.ErrInvalidAmount,
@@ -196,15 +146,6 @@ var testsCreate = []TestCreate{
 			Price:        10000,
 			Amount:       10,
 			Status:       true,
-		},
-		want: &domain.Product{
-			ID:           1,
-			Name:         "Test-product",
-			Manufacturer: "test-manufacturer",
-			Price:        10000,
-			Amount:       10,
-			Status:       true,
-			Category:     "",
 		},
 		wantErr:  true,
 		wantResp: domain.ErrInvalidCategory,
@@ -226,9 +167,7 @@ func TestCreateProduct(t *testing.T) {
 
 			productService := NewProductService(&mockRepo)
 
-			req := test.input
-
-			product, err := productService.CreateProduct(context.Background(), &req)
+			product, err := productService.CreateProduct(context.Background(), &test.input)
 
 			if test.wantErr {
 				if product != nil {
@@ -243,6 +182,443 @@ func TestCreateProduct(t *testing.T) {
 					t.Errorf("mismatch (-want +got):\n%s", diff)
 				}
 
+				if err != nil {
+					t.Fatalf("expected nil, got: %v", err)
+				}
+			}
+		})
+	}
+}
+
+type TestUpdate struct {
+	name      string
+	input     domain.UpdateProductRequest
+	productID int
+	want      *domain.Product
+	wantErr   bool
+	wantResp  error
+}
+
+var testsUpdate = []TestUpdate{
+	{
+		name: "general",
+		input: domain.UpdateProductRequest{
+			Name:         stringPtr("UPD-Test-product"),
+			Manufacturer: stringPtr("UPD-Test-manufacturer"),
+			Price:        intPtr(15000),
+			Amount:       intPtr(15),
+			Status:       boolPtr(true),
+			Category:     stringPtr("Household appliances"),
+		},
+		productID: 1,
+		want: &domain.Product{
+			ID:           1,
+			Name:         "UPD-Test-product",
+			Manufacturer: "UPD-Test-manufacturer",
+			Price:        15000,
+			Amount:       15,
+			Status:       false,
+			Category:     "Household appliances",
+		},
+		wantErr: false,
+	},
+	{
+		name: "missing values",
+		input: domain.UpdateProductRequest{
+			Name:         nil,
+			Manufacturer: nil,
+			Price:        nil,
+			Amount:       nil,
+			Status:       nil,
+			Category:     nil,
+		},
+		productID: 1,
+		wantErr:   true,
+		wantResp:  domain.ErrNoUpdateData,
+	},
+	{
+		name: "invalid name",
+		input: domain.UpdateProductRequest{
+			Name: stringPtr("t"),
+		},
+		productID: 1,
+		wantErr:   true,
+		wantResp:  domain.ErrInvalidName,
+	},
+	{
+		name: "invalid manufacturer",
+		input: domain.UpdateProductRequest{
+			Name:         stringPtr("UPD-Test-product"),
+			Manufacturer: stringPtr(""),
+		},
+		productID: 1,
+		wantErr:   true,
+		wantResp:  domain.ErrInvalidManufacturer,
+	},
+	{
+		name: "invalid price",
+		input: domain.UpdateProductRequest{
+			Name:         stringPtr("UPD-Test-product"),
+			Manufacturer: stringPtr("UPD-Test-manufacturer"),
+			Price:        intPtr(-15000),
+		},
+		productID: 1,
+		wantErr:   true,
+		wantResp:  domain.ErrInvalidPrice,
+	},
+	{
+		name: "invalid amount",
+		input: domain.UpdateProductRequest{
+			Name:         stringPtr("UPD-Test-product"),
+			Manufacturer: stringPtr("UPD-Test-manufacturer"),
+			Price:        intPtr(15000),
+			Amount:       intPtr(-15),
+		},
+		productID: 1,
+		wantErr:   true,
+		wantResp:  domain.ErrInvalidAmount,
+	},
+	{
+		name: "invalid category",
+		input: domain.UpdateProductRequest{
+			Name:         stringPtr("UPD-Test-product"),
+			Manufacturer: stringPtr("UPD-Test-manufacturer"),
+			Price:        intPtr(15000),
+			Amount:       intPtr(15),
+			Category:     stringPtr(""),
+		},
+		productID: 1,
+		wantErr:   true,
+		wantResp:  domain.ErrInvalidCategory,
+	},
+}
+
+func TestUpdateProduct(t *testing.T) {
+	for _, test := range testsUpdate {
+		t.Run(test.name, func(t *testing.T) {
+			mockRepo := MockProductRepo{
+				updateProduct: func(ctx context.Context, id int, updateData map[string]any) (*domain.Product, error) {
+					if test.wantErr {
+						return nil, nil
+					}
+
+					return test.want, nil
+				},
+			}
+
+			productService := NewProductService(&mockRepo)
+
+			product, err := productService.UpdateProduct(context.Background(), test.productID, &test.input)
+
+			if test.wantErr {
+				if product != nil {
+					t.Errorf("expected nil, got: %v", product)
+				}
+
+				if err != test.wantResp {
+					t.Fatalf("expected %v, got: %v", test.wantResp, err)
+				}
+			} else {
+				if diff := cmp.Diff(test.want, product); diff != "" {
+					t.Errorf("mismatch (-want +got):\n%s", diff)
+				}
+
+				if err != nil {
+					t.Fatalf("expected nil, got: %v", err)
+				}
+			}
+		})
+	}
+}
+
+func stringPtr(s string) *string { return &s }
+func intPtr(i int) *int          { return &i }
+func boolPtr(b bool) *bool       { return &b }
+
+type TestGet struct {
+	name      string
+	productID int
+	want      *domain.Product
+	wantErr   bool
+	wantResp  error
+}
+
+var testsGet = []TestGet{
+	{
+		name:      "general",
+		productID: 1,
+		want: &domain.Product{
+			Name:         "Test-product",
+			Manufacturer: "test-manufacturer",
+			Price:        10000,
+			Amount:       10,
+			Status:       true,
+			Category:     "Household appliances",
+		},
+		wantErr: false,
+	},
+	{
+		name:      "invalid ID",
+		productID: -1,
+		wantErr:   true,
+		wantResp:  domain.ErrInvalidID,
+	},
+	{
+		name:      "product not found",
+		productID: 1,
+		wantErr:   true,
+		wantResp:  domain.ErrProductsNotFound,
+	},
+}
+
+func TestGetProduct(t *testing.T) {
+	for _, test := range testsGet {
+		t.Run(test.name, func(t *testing.T) {
+			mockRepo := MockProductRepo{
+				getProduct: func(ctx context.Context, id int) (*domain.Product, error) {
+					if test.wantErr {
+						return nil, domain.ErrProductsNotFound
+					}
+
+					return test.want, nil
+				},
+			}
+
+			productService := NewProductService(&mockRepo)
+
+			product, err := productService.GetProduct(context.Background(), test.productID)
+
+			if test.wantErr {
+				if product != nil {
+					t.Errorf("expected nil, got: %v", product)
+				}
+
+				if err != test.wantResp {
+					t.Fatalf("expected %v, got: %v", test.wantResp, err)
+				}
+			} else {
+				if diff := cmp.Diff(test.want, product); diff != "" {
+					t.Errorf("mismatch (-want +got):\n%s", diff)
+				}
+
+				if err != nil {
+					t.Fatalf("expected nil, got: %v", err)
+				}
+			}
+		})
+	}
+}
+
+type TestList struct {
+	name           string
+	cursor         int
+	limit          uint64
+	want           []*domain.Product
+	wantPagination *domain.Pagination
+	wantErr        bool
+	wantResp       error
+}
+
+var testsList = []TestList{
+	{
+		name:   "general",
+		cursor: 1,
+		limit:  2,
+		want: []*domain.Product{
+			{
+				ID:           1,
+				Name:         "test-product",
+				Manufacturer: "test-manufacturer",
+				Price:        10000,
+				Amount:       10,
+				Status:       true,
+				Category:     "Household appliances",
+			},
+			{
+				ID:           2,
+				Name:         "test-product",
+				Manufacturer: "test-manufacturer",
+				Price:        10000,
+				Amount:       10,
+				Status:       true,
+				Category:     "Household appliances",
+			},
+			{
+				ID:           3,
+				Name:         "test-product",
+				Manufacturer: "test-manufacturer",
+				Price:        10000,
+				Amount:       10,
+				Status:       true,
+				Category:     "Household appliances",
+			},
+		},
+		wantPagination: &domain.Pagination{
+			NextCursor: 3,
+			HasMore:    true,
+			Limit:      2,
+		},
+		wantErr: false,
+	},
+	{
+		name:   "list end",
+		cursor: 2,
+		limit:  3,
+		want: []*domain.Product{
+			{
+				ID:           1,
+				Name:         "test-product",
+				Manufacturer: "test-manufacturer",
+				Price:        10000,
+				Amount:       10,
+				Status:       true,
+				Category:     "Household appliances",
+			},
+			{
+				ID:           2,
+				Name:         "test-product",
+				Manufacturer: "test-manufacturer",
+				Price:        10000,
+				Amount:       10,
+				Status:       true,
+				Category:     "Household appliances",
+			},
+			{
+				ID:           3,
+				Name:         "test-product",
+				Manufacturer: "test-manufacturer",
+				Price:        10000,
+				Amount:       10,
+				Status:       true,
+				Category:     "Household appliances",
+			},
+		},
+		wantPagination: &domain.Pagination{
+			NextCursor: 0,
+			HasMore:    false,
+			Limit:      3,
+		},
+		wantErr: false,
+	},
+	{
+		name:   "negative cursor",
+		cursor: -2,
+		limit:  2,
+		want: []*domain.Product{
+			{
+				ID:           1,
+				Name:         "test-product",
+				Manufacturer: "test-manufacturer",
+				Price:        10000,
+				Amount:       10,
+				Status:       true,
+				Category:     "Household appliances",
+			},
+			{
+				ID:           2,
+				Name:         "test-product",
+				Manufacturer: "test-manufacturer",
+				Price:        10000,
+				Amount:       10,
+				Status:       true,
+				Category:     "Household appliances",
+			},
+			{
+				ID:           3,
+				Name:         "test-product",
+				Manufacturer: "test-manufacturer",
+				Price:        10000,
+				Amount:       10,
+				Status:       true,
+				Category:     "Household appliances",
+			},
+		},
+		wantPagination: &domain.Pagination{
+			NextCursor: 3,
+			HasMore:    true,
+			Limit:      2,
+		},
+		wantErr: false,
+	},
+}
+
+func TestListProducts(t *testing.T) {
+	for _, test := range testsList {
+		t.Run(test.name, func(t *testing.T) {
+			mockRepo := MockProductRepo{
+				listProducts: func(ctx context.Context, cursor int, limit uint64) ([]*domain.Product, int, bool, error) {
+					return test.want, test.wantPagination.NextCursor, test.wantPagination.HasMore, nil
+				},
+			}
+
+			productService := NewProductService(&mockRepo)
+
+			products, pagination, err := productService.ListProducts(context.Background(), test.cursor, test.limit)
+
+			if test.wantErr {
+				if products != nil {
+					t.Errorf("expected nil, got: %v", products)
+				}
+
+				if err != test.wantResp {
+					t.Fatalf("expected %v, got: %v", test.wantResp, err)
+				}
+			} else {
+				if diff := cmp.Diff(test.want, products); diff != "" {
+					t.Errorf("mismatch (-want +got):\n%s", diff)
+				}
+
+				if diff := cmp.Diff(test.wantPagination, pagination); diff != "" {
+					t.Errorf("mismatch (-want +got):\n%s", diff)
+				}
+
+				if err != nil {
+					t.Fatalf("expected nil, got: %v", err)
+				}
+			}
+		})
+	}
+}
+
+type TestDelete struct {
+	name      string
+	productID int
+	wantErr   bool
+	wantResp  error
+}
+
+var testsDelete = []TestDelete{
+	{
+		name:      "general",
+		productID: 1,
+		wantErr:   false,
+	},
+	{
+		name:      "invalid ID",
+		productID: -1,
+		wantErr:   true,
+		wantResp:  domain.ErrInvalidID,
+	},
+}
+
+func TestDeleteProduct(t *testing.T) {
+	for _, test := range testsDelete {
+		t.Run(test.name, func(t *testing.T) {
+			mockRepo := MockProductRepo{
+				deleteProduct: func(ctx context.Context, id int) error {
+					return nil
+				},
+			}
+
+			productService := NewProductService(&mockRepo)
+
+			err := productService.DeleteProduct(context.Background(), test.productID)
+
+			if test.wantErr {
+				if err != test.wantResp {
+					t.Fatalf("expected %v, got: %v", test.wantResp, err)
+				}
+			} else {
 				if err != nil {
 					t.Fatalf("expected nil, got: %v", err)
 				}
