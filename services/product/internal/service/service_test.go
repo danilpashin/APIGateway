66095f2db32136d9c328820953141a76ceb85eb6
DanilRoiven <106142708+danilpashin@users.noreply.github.com>
2026-05-06 15:41:18 +0500

    refactor(product): simplify service tests using testify
    - Replace MockProductRepo with MockRepo using testify mock
    - Replace if statements with testify assert

--- services/product/internal/service/service_test.go
+++ services/product/internal/service/service_test.go
@@ -3,66 +3,78 @@ package service
 import (
 	"apigateway/services/product/internal/domain"
 	"context"
+	"errors"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-type MockProductRepo struct {
-	createProduct func(ctx context.Context, insertData map[string]any) (*domain.Product, error)
-	updateProduct func(ctx context.Context, id int, updateData map[string]any) (*domain.Product, error)
-	getProduct    func(ctx context.Context, id int) (*domain.Product, error)
-	listProducts  func(ctx context.Context, cursor int, limit uint64) ([]*domain.Product, int, bool, error)
-	deleteProduct func(ctx context.Context, id int) error
+type MockRepo struct {
+	mock.Mock
 }
 
-func (m *MockProductRepo) CreateProduct(ctx context.Context, insertData map[string]any) (*domain.Product, error) {
-	if m.createProduct != nil {
-		return m.createProduct(ctx, insertData)
-	}
-	return nil, nil
+func (m *MockRepo) CreateProduct(ctx context.Context, insertData map[string]any) (*domain.Product, error) {
+	args := m.Called(ctx, insertData)
+	p, _ := args[0].(*domain.Product)
+	return p, args.Error(1)
 }
 
-func (m *MockProductRepo) UpdateProduct(ctx context.Context, id int, updateData map[string]any) (*domain.Product, error) {
-	if m.updateProduct != nil {
-		return m.updateProduct(ctx, id, updateData)
-	}
-	return nil, nil
+func (m *MockRepo) UpdateProduct(ctx context.Context, id int, updateData map[string]any) (*domain.Product, error) {
+	args := m.Called(ctx, id, updateData)
+	p, _ := args[0].(*domain.Product)
+	return p, args.Error(1)
 }
 
-func (m *MockProductRepo) GetProduct(ctx context.Context, id int) (*domain.Product, error) {
-	if m.getProduct != nil {
-		return m.getProduct(ctx, id)
-	}
-	return nil, nil
+func (m *MockRepo) GetProduct(ctx context.Context, id int) (*domain.Product, error) {
+	args := m.Called(ctx, id)
+	p, _ := args[0].(*domain.Product)
+	return p, args.Error(1)
 }
 
-func (m *MockProductRepo) ListProducts(ctx context.Context, cursor int, limit uint64) ([]*domain.Product, int, bool, error) {
-	if m.listProducts != nil {
-		return m.listProducts(ctx, cursor, limit)
-	}
-	return nil, 0, false, nil
+func (m *MockRepo) ListProducts(ctx context.Context, cursor int, limit uint64) ([]*domain.Product, int, bool, error) {
+	args := m.Called(ctx, cursor, limit)
+	p, _ := args[0].([]*domain.Product)
+	return p, args.Int(1), args.Bool(2), args.Error(3)
 }
 
-func (m *MockProductRepo) DeleteProduct(ctx context.Context, id int) error {
-	if m.deleteProduct != nil {
-		return m.deleteProduct(ctx, id)
-	}
-	return nil
+func (m *MockRepo) DeleteProduct(ctx context.Context, id int) error {
+	args := m.Called(ctx, id)
+	return args.Error(0)
 }
 
 type TestCreate struct {
-	name     string
-	input    domain.CreateProductRequest
-	want     *domain.Product
-	wantErr  bool
-	wantResp error
+	name       string
+	input      domain.CreateProductRequest
+	mockInput  map[string]any
+	mockReturn *domain.Product
+	mockError  error
+	want       *domain.Product
+	wantErr    bool
+	wantResp   error
 }
 
 var testsCreate = []TestCreate{
 	{
 		name: "success: all values",
 		input: domain.CreateProductRequest{
+			Name:         "Test-product",
+			Manufacturer: "test-manufacturer",
+			Price:        10000,
+			Amount:       10,
+			Status:       true,
+			Category:     "Household appliances",
+		},
+		mockInput: map[string]any{
+			"name":         "Test-product",
+			"manufacturer": "test-manufacturer",
+			"price":        10000,
+			"amount":       10,
+			"status":       true,
+			"category":     "Household appliances",
+		},
+		mockReturn: &domain.Product{
+			ID:           1,
 			Name:         "Test-product",
 			Manufacturer: "test-manufacturer",
 			Price:        10000,
@@ -171,50 +183,38 @@ var testsCreate = []TestCreate{
 }
 
 func TestProductService_Create(t *testing.T) {
-	for _, test := range testsCreate {
-		t.Run(test.name, func(t *testing.T) {
-			mockRepo := MockProductRepo{
-				createProduct: func(ctx context.Context, insertData map[string]any) (*domain.Product, error) {
-					if test.wantErr {
-						return nil, nil
-					}
-
-					return test.want, nil
-				},
+	for _, tt := range testsCreate {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := new(MockRepo)
+			if !tt.wantErr {
+				mockRepo.On("CreateProduct", mock.Anything, tt.mockInput).Return(tt.mockReturn, tt.wantResp)
 			}
 
-			productService := NewProductService(&mockRepo)
+			productService := NewProductService(mockRepo)
 
-			product, err := productService.CreateProduct(context.Background(), &test.input)
+			product, err := productService.CreateProduct(context.Background(), &tt.input)
 
-			if test.wantErr {
-				if product != nil {
-					t.Errorf("expected nil, got: %v", product)
-				}
-
-				if err != test.wantResp || err == nil {
-					t.Fatalf("expected %v, got: %v", test.wantResp, err)
-				}
+			if tt.wantErr {
+				assert.Equal(t, tt.want, product)
+				assert.Equal(t, tt.wantResp, err)
 			} else {
-				if diff := cmp.Diff(test.want, product); diff != "" {
-					t.Errorf("mismatch (-want +got):\n%s", diff)
-				}
-
-				if err != nil {
-					t.Fatalf("expected nil, got: %v", err)
-				}
+				assert.Equal(t, tt.want, product)
+				assert.Equal(t, nil, err)
 			}
+			mockRepo.AssertExpectations(t)
 		})
 	}
 }
 
 type TestUpdate struct {
-	name      string
-	input     domain.UpdateProductRequest
-	productID int
-	want      *domain.Product
-	wantErr   bool
-	wantResp  error
+	name       string
+	input      domain.UpdateProductRequest
+	productID  int
+	mockInput  map[string]any
+	mockReturn *domain.Product
+	want       *domain.Product
+	wantErr    bool
+	wantResp   error
 }
 
 var testsUpdate = []TestUpdate{
@@ -229,13 +229,30 @@ var testsUpdate = []TestUpdate{
 			Category:     stringPtr("Household appliances"),
 		},
 		productID: 1,
+		mockInput: map[string]any{
+			"name":         "UPD-Test-product",
+			"manufacturer": "UPD-Test-manufacturer",
+			"price":        15000,
+			"amount":       15,
+			"status":       true,
+			"category":     "Household appliances",
+		},
+		mockReturn: &domain.Product{
+			ID:           1,
+			Name:         "UPD-Test-product",
+			Manufacturer: "UPD-Test-manufacturer",
+			Price:        15000,
+			Amount:       15,
+			Status:       true,
+			Category:     "Household appliances",
+		},
 		want: &domain.Product{
 			ID:           1,
 			Name:         "UPD-Test-product",
 			Manufacturer: "UPD-Test-manufacturer",
 			Price:        15000,
 			Amount:       15,
-			Status:       false,
+			Status:       true,
 			Category:     "Household appliances",
 		},
 		wantErr: false,
@@ -312,39 +329,25 @@ var testsUpdate = []TestUpdate{
 }
 
 func TestProductService_Update(t *testing.T) {
-	for _, test := range testsUpdate {
-		t.Run(test.name, func(t *testing.T) {
-			mockRepo := MockProductRepo{
-				updateProduct: func(ctx context.Context, id int, updateData map[string]any) (*domain.Product, error) {
-					if test.wantErr {
-						return nil, nil
-					}
-
-					return test.want, nil
-				},
+	for _, tt := range testsUpdate {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := new(MockRepo)
+			if !tt.wantErr {
+				mockRepo.On("UpdateProduct", mock.Anything, tt.productID, tt.mockInput).Return(tt.mockReturn, tt.wantResp)
 			}
 
-			productService := NewProductService(&mockRepo)
+			productService := NewProductService(mockRepo)
 
-			product, err := productService.UpdateProduct(context.Background(), test.productID, &test.input)
+			product, err := productService.UpdateProduct(context.Background(), tt.productID, &tt.input)
 
-			if test.wantErr {
-				if product != nil {
-					t.Errorf("expected nil, got: %v", product)
-				}
-
-				if err != test.wantResp {
-					t.Fatalf("expected %v, got: %v", test.wantResp, err)
-				}
+			if tt.wantErr {
+				assert.Equal(t, tt.want, product)
+				assert.Equal(t, tt.wantResp, err)
 			} else {
-				if diff := cmp.Diff(test.want, product); diff != "" {
-					t.Errorf("mismatch (-want +got):\n%s", diff)
-				}
-
-				if err != nil {
-					t.Fatalf("expected nil, got: %v", err)
-				}
+				assert.Equal(t, tt.want, product)
+				assert.Equal(t, nil, err)
 			}
+			mockRepo.AssertExpectations(t)
 		})
 	}
 }
@@ -354,17 +357,26 @@ func intPtr(i int) *int          { return &i }
 func boolPtr(b bool) *bool       { return &b }
 
 type TestGet struct {
-	name      string
-	productID int
-	want      *domain.Product
-	wantErr   bool
-	wantResp  error
+	name       string
+	productID  int
+	mockReturn *domain.Product
+	want       *domain.Product
+	wantErr    bool
+	wantResp   error
 }
 
 var testsGet = []TestGet{
 	{
 		name:      "success",
 		productID: 1,
+		mockReturn: &domain.Product{
+			Name:         "Test-product",
+			Manufacturer: "test-manufacturer",
+			Price:        10000,
+			Amount:       10,
+			Status:       true,
+			Category:     "Household appliances",
+		},
 		want: &domain.Product{
 			Name:         "Test-product",
 			Manufacturer: "test-manufacturer",
@@ -381,48 +393,28 @@ var testsGet = []TestGet{
 		wantErr:   true,
 		wantResp:  domain.ErrInvalidID,
 	},
-	{
-		name:      "error: product not found",
-		productID: 1,
-		wantErr:   true,
-		wantResp:  domain.ErrProductsNotFound,
-	},
 }
 
 func TestProductService_Get(t *testing.T) {
-	for _, test := range testsGet {
-		t.Run(test.name, func(t *testing.T) {
-			mockRepo := MockProductRepo{
-				getProduct: func(ctx context.Context, id int) (*domain.Product, error) {
-					if test.wantErr {
-						return nil, domain.ErrProductsNotFound
-					}
-
-					return test.want, nil
-				},
+	for _, tt := range testsGet {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := new(MockRepo)
+			if !tt.wantErr {
+				mockRepo.On("GetProduct", mock.Anything, tt.productID).Return(tt.mockReturn, tt.wantResp)
 			}
 
-			productService := NewProductService(&mockRepo)
+			productService := NewProductService(mockRepo)
 
-			product, err := productService.GetProduct(context.Background(), test.productID)
+			product, err := productService.GetProduct(context.Background(), tt.productID)
 
-			if test.wantErr {
-				if product != nil {
-					t.Errorf("expected nil, got: %v", product)
-				}
-
-				if err != test.wantResp {
-					t.Fatalf("expected %v, got: %v", test.wantResp, err)
-				}
+			if tt.wantErr {
+				assert.Equal(t, tt.want, product)
+				assert.Equal(t, tt.wantResp, err)
 			} else {
-				if diff := cmp.Diff(test.want, product); diff != "" {
-					t.Errorf("mismatch (-want +got):\n%s", diff)
-				}
-
-				if err != nil {
-					t.Fatalf("expected nil, got: %v", err)
-				}
+				assert.Equal(t, tt.want, product)
+				assert.Equal(t, nil, err)
 			}
+			mockRepo.AssertExpectations(t)
 		})
 	}
 }
@@ -431,6 +423,10 @@ type TestList struct {
 	name           string
 	cursor         int
 	limit          uint64
+	mockNextCursor int
+	mockHasMore    bool
+	mockCursor     int
+	mockLimit      uint64
 	want           []*domain.Product
 	wantPagination *domain.Pagination
 	wantErr        bool
@@ -439,9 +435,13 @@ type TestList struct {
 
 var testsList = []TestList{
 	{
-		name:   "success: first two products (cursor=1, limit=2)",
-		cursor: 1,
-		limit:  2,
+		name:           "success: first two products (cursor=1, limit=2)",
+		cursor:         1,
+		limit:          2,
+		mockNextCursor: 3,
+		mockHasMore:    true,
+		mockCursor:     1,
+		mockLimit:      2,
 		want: []*domain.Product{
 			{
 				ID:           1,
@@ -479,9 +479,11 @@ var testsList = []TestList{
 		wantErr: false,
 	},
 	{
-		name:   "success: end of list (no more)",
-		cursor: 3,
-		limit:  3,
+		name:       "success: end of list (no more)",
+		cursor:     3,
+		limit:      3,
+		mockCursor: 3,
+		mockLimit:  3,
 		want: []*domain.Product{
 			{
 				ID:           1,
@@ -519,9 +521,11 @@ var testsList = []TestList{
 		wantErr: false,
 	},
 	{
-		name:   "success: negative cursor and null limit",
-		cursor: -2,
-		limit:  0,
+		name:       "success: negative cursor and null limit",
+		cursor:     -2,
+		limit:      0,
+		mockCursor: 0,
+		mockLimit:  10,
 		want: []*domain.Product{
 			{
 				ID:           1,
@@ -559,9 +563,11 @@ var testsList = []TestList{
 		wantErr: false,
 	},
 	{
-		name:   "success: limit > 50 clamped to 50",
-		cursor: 1,
-		limit:  100,
+		name:       "success: limit > 50 clamped to 50",
+		cursor:     1,
+		limit:      100,
+		mockCursor: 1,
+		mockLimit:  50,
 		want: []*domain.Product{
 			{
 				ID:           1,
@@ -599,51 +605,35 @@ var testsList = []TestList{
 		wantErr: false,
 	},
 	{
-		name:    "error: repository error",
-		cursor:  1,
-		limit:   2,
-		wantErr: true,
+		name:       "error: repository error",
+		cursor:     1,
+		limit:      2,
+		mockCursor: 1,
+		mockLimit:  2,
+		wantErr:    true,
+		wantResp:   errors.New("repo error"),
 	},
 }
 
 func TestProductService_List(t *testing.T) {
-	for _, test := range testsList {
-		t.Run(test.name, func(t *testing.T) {
-			mockRepo := MockProductRepo{
-				listProducts: func(ctx context.Context, cursor int, limit uint64) ([]*domain.Product, int, bool, error) {
-					if test.wantErr {
-						return nil, 0, false, domain.ErrListQuery
-					}
+	for _, tt := range testsList {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := new(MockRepo)
+			mockRepo.On("ListProducts", mock.Anything, tt.mockCursor, tt.mockLimit).Return(tt.want, tt.mockNextCursor, tt.mockHasMore, tt.wantResp)
 
-					return test.want, test.wantPagination.NextCursor, test.wantPagination.HasMore, nil
-				},
-			}
+			productService := NewProductService(mockRepo)
 
-			productService := NewProductService(&mockRepo)
+			products, pagination, err := productService.ListProducts(context.Background(), tt.cursor, tt.limit)
 
-			products, pagination, err := productService.ListProducts(context.Background(), test.cursor, test.limit)
-
-			if test.wantErr {
-				if products != nil {
-					t.Errorf("expected nil, got: %v", products)
-				}
-
-				if err == nil {
-					t.Fatalf("expected nil, got: %v", err)
-				}
+			if tt.wantErr {
+				assert.Equal(t, tt.want, products)
+				assert.Equal(t, tt.wantResp, err)
 			} else {
-				if diff := cmp.Diff(test.want, products); diff != "" {
-					t.Errorf("mismatch (-want +got):\n%s", diff)
-				}
-
-				if diff := cmp.Diff(test.wantPagination, pagination); diff != "" {
-					t.Errorf("mismatch (-want +got):\n%s", diff)
-				}
-
-				if err != nil {
-					t.Fatalf("expected nil, got: %v", err)
-				}
+				assert.Equal(t, tt.want, products)
+				assert.Equal(t, tt.wantPagination, pagination)
+				assert.Equal(t, nil, err)
 			}
+			mockRepo.AssertExpectations(t)
 		})
 	}
 }
@@ -670,27 +660,23 @@ var testsDelete = []TestDelete{
 }
 
 func TestProductService_Delete(t *testing.T) {
-	for _, test := range testsDelete {
-		t.Run(test.name, func(t *testing.T) {
-			mockRepo := MockProductRepo{
-				deleteProduct: func(ctx context.Context, id int) error {
-					return nil
-				},
+	for _, tt := range testsDelete {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := new(MockRepo)
+			if !tt.wantErr {
+				mockRepo.On("DeleteProduct", mock.Anything, tt.productID).Return(tt.wantResp)
 			}
 
-			productService := NewProductService(&mockRepo)
+			productService := NewProductService(mockRepo)
 
-			err := productService.DeleteProduct(context.Background(), test.productID)
+			err := productService.DeleteProduct(context.Background(), tt.productID)
 
-			if test.wantErr {
-				if err != test.wantResp {
-					t.Fatalf("expected %v, got: %v", test.wantResp, err)
-				}
+			if tt.wantErr {
+				assert.Equal(t, tt.wantResp, err)
 			} else {
-				if err != nil {
-					t.Fatalf("expected nil, got: %v", err)
-				}
+				assert.Equal(t, nil, err)
 			}
+			mockRepo.AssertExpectations(t)
 		})
 	}
 }
